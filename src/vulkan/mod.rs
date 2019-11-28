c1d410394894023bc1abb4a5c18e998d56961213
Szymon Zyguła <zbrojny120@protonmail.ch>
2019-11-28 22:15:45 +0100

    Break errors into multiple lines

--- src/vulkan/mod.rs
+++ src/vulkan/mod.rs
@@ -3,14 +3,22 @@ use ash::{self, vk};
 use crate::builder::BuilderError;
 
 custom_error!{pub VulkanError
-    OperationFailed {source: vk::Result, operation: String} = "operation failed: {operation} ({source})",
-    LibraryLoadError {source: ash::LoadingError} = "failed to load Vulkan library: {source}",
-    ValidationLayersNotAvailable = "specified validation layers are not available",
-    InstanceCreateError {source: ash::InstanceError} = "failed to create vulkan instance: {source}",
-    VulkanBuildError {source: BuilderError} = "failed to build a Vulkan structure",
-    InstanceExtensionsCreationError {source: std::ffi::NulError} = "failed to create C-like nul-terminated string: {source}",
-    SuitableDeviceNotFound = "failed to find a physical device fulfilling all criteria",
-    QueueFamilyNotSupported {queue: physical_device::QueueFamily} = "physical device was asked about an index of a queue family that it does not support"
+    OperationFailed {source: vk::Result, operation: String} =
+        "operation failed: {operation} ({source})",
+    LibraryLoadError {source: ash::LoadingError} =
+        "failed to load Vulkan library: {source}",
+    ValidationLayersNotAvailable =
+        "specified validation layers are not available",
+    InstanceCreateError {source: ash::InstanceError} =
+        "failed to create vulkan instance: {source}",
+    VulkanBuildError {source: BuilderError} =
+        "failed to build a Vulkan structure",
+    InstanceExtensionsCreationError {source: std::ffi::NulError} =
+        "failed to create C-like nul-terminated string: {source}",
+    SuitableDeviceNotFound =
+        "failed to find a physical device fulfilling all criteria",
+    QueueFamilyNotSupported {queue: physical_device::QueueFamily} =
+        "physical device was asked about an index of a queue family that it does not support"
 }
 
 impl VulkanError {
